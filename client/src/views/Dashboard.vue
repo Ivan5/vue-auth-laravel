012254e8db00f18b53b29925f9637cda6ae45853
Ivan5 <converse_ivan@hotmail.com>
2019-10-31 17:14:38 -0600

    Add a axios to make a request of api routes

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div class="dashboard">
+    <h1>Dashboard</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "dashboard"
 };
 </script>
